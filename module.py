036c75467eee5497f52a681c5cb93b110c869f3a
BHSAditya <83492166+BHSAditya@users.noreply.github.com>
2023-09-10 21:48:52 +0530

    Update module.py

--- module.py
+++ module.py
@@ -194,6 +194,24 @@ def gaussianBlur(img):
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
+def alpha_blurring():
+    import cv2
+    img1 = cv2.imread('RESOURCES/Photos/cat.jpg')
+    img2 = cv2.imread('RESOURCES/Photos/park.jpg')
+    img2 = cv2.resize(img2, img1.shape[1::-1])
+    cv2.imshow("img 1",img1)
+    cv2.waitKey(0)
+    cv2.imshow("img 2",img2)
+    cv2.waitKey(0)
+    choice = 1
+    while (choice):
+        alpha = float(input("Enter alpha value(from 0-1): "))
+        dst = cv2.addWeighted(img1, alpha , img2, 1-alpha, 0)
+        cv2.imwrite('alpha_mask_.png', dst)
+        img3 = cv2.imread('alpha_mask_.png')
+        cv2.imshow("alpha blending 1",img3)
+        cv2.waitKey(0)
+        choice = int(input("Enter 1 to continue and 0 to exit: "))
 
 
 def contour_detection(im):
